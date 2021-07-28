ac8af38d5feb48d333762febb227a8ce935ea55f
Ashton Walden <ashtonwalden@icloud.com>
2021-07-28 02:31:59 -0600

    alternates x and o in onclick function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+var markers = ["X", "O"];
+
+var playerTurn = 0;
+
 function gameMove(selectedDiv) {
-    selectedDiv.innerText = "X";
+    selectedDiv.innerText = markers[playerTurn];
+
+    if (playerTurn ==0) {
+        playerTurn = 1;
+    } else {
+        playerTurn = 0;
+    }
 }
