78ddfb805a41d48d8e756da231674619a52d73a1
Ashton Walden <ashtonwalden@icloud.com>
2021-07-29 00:14:48 -0600

    added value to spaces and adds to player's total score when clicked

--- js/script.js
+++ js/script.js
@@ -5,11 +5,19 @@ var players = []
 players[0] = prompt("Enter player 1");
 players[1] = prompt("Enter player 2");
 
+var scores = [0, 0];
+
 var playerTurn = 0;
 
-function gameMove(selectedDiv) {
+function gameMove(selectedDiv, divValue) {
     // selects div clicked and adds correct marker inside
     selectedDiv.innerText = markers[playerTurn];
+
+    // adds values to each player based on space selected
+    scores[playerTurn] += divValue;
+    console.log(players[0] + " score: " + scores[0]);
+    console.log(players[1] + " score: " + scores[1]);
+
 
     // alternates values of player turn and changes marker
     if (playerTurn ==0) {
@@ -21,6 +29,7 @@ function gameMove(selectedDiv) {
     // sets on click to be empty so the div can't be changed after it's been clicked
     selectedDiv.onclick = '';
 
+    // displays message showing current players turn
     document.getElementById("turn-message").innerText = players[playerTurn] + "'s Turn."
 }
 
