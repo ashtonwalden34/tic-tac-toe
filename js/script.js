36a800e47da3c071146eb79362ab737c97848fea
Ashton Walden <ashtonwalden@icloud.com>
2021-07-29 01:38:23 -0600

    game logic fully built

--- js/script.js
+++ js/script.js
@@ -2,41 +2,51 @@ var markers = ["X", "O"];
 var players = []
     players[0] = prompt("Enter player 1");
     players[1] = prompt("Enter player 2");
-var scores = [0, 0];
+var scores = [];
 var winValues = [7, 56, 73, 84, 146, 273, 292, 448];
 var playerTurn = 0;
+var gameOver;
 
 
 function gameMove(selectedDiv, divValue) {
-    // selects div clicked and adds correct marker inside
-    selectedDiv.innerText = markers[playerTurn];
+    // if gameOver is not true then run function
+    if (!gameOver) {
+        // selects div clicked and adds correct marker inside
+        selectedDiv.innerText = markers[playerTurn];
 
-    // adds values to each player based on space selected
-    scores[playerTurn] += divValue;
-    // console.log(players[0] + " score: " + scores[0]);
-    // console.log(players[1] + " score: " + scores[1]);
-    
-    // call win function, checks for win and displays win message if win condition is met
-    if (winCondition()){
-        document.getElementById("game-message").innerText = players[playerTurn] + " wins!"
-        // only continues game based if the win condition is not met
-    } else {
-        // alternates values of player turn and changes marker
-        if (playerTurn ==0) {
-            playerTurn = 1;
+        // adds values to each player based on space selected
+        scores[playerTurn] += divValue;
+        // console.log(players[0] + " score: " + scores[0]);
+        // console.log(players[1] + " score: " + scores[1]);
+        
+        // call win function, checks for win and displays win message if win condition is met
+        if (winCondition()){
+            document.getElementById("game-message").innerText = players[playerTurn] + " wins!"
+            // only continues game based if the win condition is not met
+        }else if (gameOver) {
+            document.getElementById("game-message").innerText = "Tie Game!"
         } else {
-            playerTurn = 0;
-        }
+            // alternates values of player turn and changes marker
+            if (playerTurn ==0) {
+                playerTurn = 1;
+            } else {
+                playerTurn = 0;
+            }
 
-        // sets on click to be empty so the div can't be changed after it's been clicked
-        selectedDiv.onclick = '';
+            // sets on click to be empty so the div can't be changed after it's been clicked
+            selectedDiv.onclick = '';
 
-        // displays message showing current players turn
-        document.getElementById("game-message").innerText = players[playerTurn] + "'s Turn."
+            // displays message showing current players turn
+            document.getElementById("game-message").innerText = players[playerTurn] + "'s Turn."
+        } 
     } 
 }
 
 function startGame() {
+        // sets scores array to hold 0, 0
+        scores = [0, 0];
+        // sets gameOver to false
+        gameOver = false;
         // grabs player from players array and displays message showing it's their turn
         document.getElementById("game-message").innerText = players[playerTurn] + "'s Turn."
 }
@@ -46,8 +56,32 @@ function startGame() {
 function winCondition() {
     for (i = 0; i < winValues.length; i++) {
         if ((scores[playerTurn] & winValues[i]) == winValues[i]) {
+            gameOver = true;
             return true;
         }
     }
+    // board is filled then gameOver variable is set to true
+    if (scores[0] + scores[1] == 511) {
+        gameOver = true;
+    }
+
     return false;
+}
+
+function restartGame() {
+    var board = document.getElementById("game-board");
+    var innerDivs = "";
+    var counter = 1;
+
+    for (i = 1; i <=3; i++) {
+        innerDivs += '<tr id="row-' + i + '">'
+        for (j = 1; j <=3; j++) {
+            innerDivs += '<th onclick="gameMove(this,' +  counter + ')"></th>'
+            counter *=2;
+        }
+        innerDivs += '</tr>';
+    }
+    board.innerHTML = innerDivs;
+
+    startGame();
 }
